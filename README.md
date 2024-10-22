4c8c74317b099978f0c22c5fe95759cfe9bd415a
OttoDyva <cph-ot@cphbusiness.dk>
2024-10-22 12:46:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Startcode for Java Deep Dive 2 Week
+
+Includes:
+
+- pom.xml
+- HibernateConfig
+- .gitignore
---
+++ src/main/java/dat/Main.java
@@ -0,0 +1,12 @@
+package dat;
+
+import dat.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("persons");
+
+        
+    }
+}
