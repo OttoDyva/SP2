61c8747a3bea622fcbf3545a09bc7e5736d6b474
OttoDyva <cph-ot@cphbusiness.dk>
2024-10-22 12:57:28 +0200

    Adding more template

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("persons");
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
 
         
     }
