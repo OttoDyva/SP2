9fa936e2dd16c136e3d2aad877858cb733661b1d
OttoDyva <cph-ot@cphbusiness.dk>
2024-10-23 16:18:08 +0200

    AuthorControllerTest

--- src/test/java/dat/controllers/AuthorControllerTest.java
+++ src/test/java/dat/controllers/AuthorControllerTest.java
@@ -1,15 +1,13 @@
 package dat.controllers;
 
 import dat.config.HibernateConfig;
+import dat.config.Populate;
 import dat.daos.AuthorDAO;
 import dat.dtos.AuthorDTO;
 import dat.entities.Author;
 import io.javalin.http.Context;
 import jakarta.persistence.EntityManagerFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,22 +15,26 @@ import java.util.List;
 import static io.restassured.RestAssured.when;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthorControllerTest {
 
-    private static EntityManagerFactory emf;
+    private EntityManagerFactory emf;
     private AuthorDAO authorDAO;
-    private static Context ctx;
     private AuthorController authorController;
+
+    private Context ctx;
 
 
     @BeforeAll
-    public static void setup() {
+    void setup() {
         emf = HibernateConfig.getEntityManagerFactoryForTest();
+        authorDAO = AuthorDAO.getInstance(emf);
+        Populate.main(null);
 
     }
 
     @AfterAll
-    public static void tearDown() {
+    void tearDown() {
         if (emf != null) {
             emf.close();
         }
@@ -40,6 +42,43 @@ class AuthorControllerTest {
 
     @Test
     public void testFindAuthorById() {
+        /*
+        ctx.pathParam("1");
+
+        AuthorController authorController = new AuthorController();
+
+        authorController.findAuthorById(ctx);
+
+        assertEquals(200, ctx.res().getStatus());
+        AuthorDTO authorDTO = (AuthorDTO) ctx.json(AuthorDTO.class);
+        assertNotNull(authorDTO);
+        assertEquals(1, authorDTO.getId());
+
+         */
+
+        /*
+        AuthorDTO request = AuthorDTO.builder()
+                .id(10)
+                .name("Author name")
+                .description("Author description")
+                .build();
+        AuthorDTO response = AuthorDTO.builder()
+                .id(11)
+                .name("Author name")
+                .description("Author description")
+                .build();
+
+        when(ctx.pathParamAsClass("id", Integer.class)).thenReturn(1);
+        when(authorDAO.findAuthorById(1)).thenReturn(response);
+        when(ctx.bodyValidator(AuthorDTO.class)).thenReturn(Validator.create(request));
+        when(authorDAO.create(request)).thenReturn(response);
+
+        authorController.create(ctx);
+
+        verify(ctx).status(201);  // Status 201 for created
+        verify(ctx).json(response);  // JSON response with created author
+
+         */
 
     }
 
